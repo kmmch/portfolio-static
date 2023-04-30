90baebe259b6b65a946ec3c3cf69e4e9e6bae3cc
kmmch <kmmch.tech@gmail.com>
2023-04-30 12:25:15 +0900

    ヘッダー改善

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = () => {
     
     // ハンバーガーボタンの要素を取得
     var headerHamburgerBtn = document.getElementById('humburger-btn');
@@ -20,3 +20,45 @@ window.onload = function(){
 
     });
 }
+
+
+
+(function() {
+
+    const fixedHeader = document.getElementById('header--fixed');
+    const humburgerBtn = document.getElementById('humburger-btn');
+    
+    const isUp = (function() {
+        
+        const scrollElement = document.scrollingElement;
+        let flag, prePoint, scrollPoint;
+        
+        return function() {
+    
+            scrollPoint = scrollElement.scrollTop;
+            flag = prePoint > scrollPoint ? true : false;
+            prePoint = scrollPoint;
+            return flag;
+    
+        }
+    
+    }());
+
+    
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 300) {
+            
+            if (isUp()) {
+                fixedHeader.classList.remove('is-show');
+                humburgerBtn.classList.remove('is-show');
+            } else {
+                fixedHeader.classList.add('is-show');
+                humburgerBtn.classList.add('is-show');
+            }
+
+        } else {
+            fixedHeader.classList.remove('is-show');
+            humburgerBtn.classList.remove('is-show');
+        }
+    })
+}());
